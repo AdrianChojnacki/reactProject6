ea30b7a3d158673f468e98a829841e03e3fa7037
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-10 19:39:32 +0200

    expanded functionalityfunctionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// CURRENCY COMPONENT
 const Currency = (props) => {
-  const value = (props.cash / props.ratio).toFixed(2);
+  const value = ((props.cash / props.ratio) * props.price).toFixed(2);
 
   return (
     <div>
@@ -8,34 +9,82 @@ const Currency = (props) => {
   );
 };
 
+// MAIN COMPONENT
 class ExchangeCounter extends React.Component {
   state = {
     amount: "",
+    product: "electricity",
   };
 
-  currencies = [
-    { id: 1, name: "dollar", ratio: 3.8, title: "$:" },
-    { id: 2, name: "euro", ratio: 4.5, title: "€:" },
-    { id: 3, name: "pound", ratio: 5.2, title: "£:" },
-  ];
+  static defaultProps = {
+    currencies: [
+      { id: 0, name: "zloty", ratio: 1, title: "zł:" },
+      { id: 1, name: "dollar", ratio: 3.8, title: "$:" },
+      { id: 2, name: "euro", ratio: 4.5, title: "€:" },
+      { id: 3, name: "pound", ratio: 5.2, title: "£:" },
+    ],
+    prices: {
+      electricity: 0.5,
+      gas: 5,
+      oranges: 3.5,
+    },
+  };
 
+  // SELECT PRODUCT PRICE
+  selectPrice(select) {
+    return this.props.prices[select];
+  }
+
+  // SELECT PRODUCT FUNCTION
+  handleSelect = (e) => {
+    this.setState({
+      amount: "",
+      product: e.target.value,
+    });
+  };
+
+  // INPUT CHANGE FUNCTION
   handleChange = (e) => {
     this.setState({
       amount: e.target.value,
     });
   };
 
-  render() {
-    const { amount } = this.state;
+  // SWITCH INPUT SUFFIX
+  insertSuffix(select) {
+    if (select === "electricity") return <em>kWh</em>;
+    else if (select === "gas") return <em>l</em>;
+    else if (select === "oranges") return <em>kg</em>;
+  }
 
-    const currencies = this.currencies.map((currency) => (
-      <Currency key={currency.id} text={currency.title} cash={amount} ratio={currency.ratio} />
+  render() {
+    const { amount, product } = this.state;
+
+    // MAP ON CURRENCIES ARRAY TO RETURN ALL 'CURRENCY' COMPONENTS
+    const currencies = this.props.currencies.map((currency) => (
+      <Currency
+        key={currency.id}
+        text={currency.title}
+        ratio={currency.ratio}
+        cash={amount}
+        price={this.selectPrice(product)}
+      />
     ));
 
     return (
       <div className="app">
         <label>
+          Wybierz produkt:
+          <select value={product} onChange={this.handleSelect}>
+            <option value="electricity">prąd</option>
+            <option value="gas">benzyna</option>
+            <option value="oranges">pomarańcze</option>
+          </select>
+        </label>
+        <br />
+        <label>
           <input value={amount} onChange={this.handleChange} type="number" />
+          {this.insertSuffix(product)}
         </label>
         {currencies}
       </div>
