0500744fd343455f35924f82c8b40692d3981e1d
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-10 18:00:51 +0200

    functionality

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
+const Dollars = (props) => {
+  const value = (props.cash / props.ratio).toFixed(2);
+
+  return <div>Wartość w dolarach: {props.cash <= 0 ? "" : value}</div>;
+};
+
+const Euros = (props) => {
+  const value = (props.cash / props.ratio).toFixed(2);
+
+  return <div>Wartość w euro: {props.cash <= 0 ? "" : value}</div>;
+};
+
 class ExchangeCounter extends React.Component {
+  state = {
+    amount: "",
+    ratioDollar: 3.8,
+    ratioEuro: 4.5,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      amount: e.target.value,
+    });
+  };
+
   render() {
-    return <div>Kantor</div>;
+    const { amount, ratioDollar, ratioEuro } = this.state;
+
+    return (
+      <div className="app">
+        <label>
+          <input value={amount} onChange={this.handleChange} type="number" />
+        </label>
+        <Dollars cash={amount} ratio={ratioDollar} />
+        <Euros cash={amount} ratio={ratioEuro} />
+      </div>
+    );
   }
 }
 
